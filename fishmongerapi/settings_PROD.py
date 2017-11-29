a7790494329ec9f20fc9b256f2945c5203e3e372
mike <mike@marketfresh.com.sg>
2017-11-29 14:26:51 +0800

    fix settings_PROD.py

--- fishmongerapi/settings_PROD.py
+++ fishmongerapi/settings_PROD.py
@@ -84,6 +84,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    #marketfresh.mysql.pythonanywhere-services.com
+    #marketfresh
+    #PansFish.2017
 }
 
 
@@ -100,24 +103,34 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
-ROOT_URLCONF = 'fishmongerapi.urls'
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'Asia/Singapore'
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/marketfresh/fishmonger/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/') #add STATIC_ROOT to DIRS
+    os.path.join(BASE_DIR, 'static/')  # add STATIC_ROOT to DIRS
 ]
 
 LOGIN_REDIRECT_URL = '/home/'

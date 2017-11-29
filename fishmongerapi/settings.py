12aee4ca0fd29157e57d6d0a8c30717a6092c46c
mike <mike@marketfresh.com.sg>
2017-11-29 13:10:52 +0800

    date fixes and input type to tel

--- fishmongerapi/settings.py
+++ fishmongerapi/settings.py
@@ -84,6 +84,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    #marketfresh.mysql.pythonanywhere-services.com
+    #marketfresh
+    #PansFish.2017
 }
 
 
